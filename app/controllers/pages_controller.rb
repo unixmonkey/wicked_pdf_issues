22934ce1d47440598f0ebcc8e8fee5baa23ef8d3
David Jones <unixmonkey1@gmail.com>
2014-08-16 12:19:11 -0400

    Get cookie from the system and show

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,10 +1,12 @@
 class PagesController < ApplicationController
 
   def issue_326
+    @remember_token = CGI.escape(cookies['remember_token'].to_s)
     respond_to do |format|
       format.html
       format.pdf do
-        render pdf: 'issue_326'
+        render pdf: 'issue_326',
+          cookie: "_session_id #{@remember_token}"
       end
     end
   end
